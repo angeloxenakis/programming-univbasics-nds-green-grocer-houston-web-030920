153ab0c20f9659f8afd093a5040626b0828d4c47
Angelo Xenakis <angeloxenakis@gmail.com>
2020-02-18 17:31:06 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -70,10 +70,12 @@ end
   # REMEMBER: This method **should** update cart
 
 def apply_clearance(cart)
+  
+
+end
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
-end
 
 def checkout(cart, coupons)
   # Consult README for inputs and outputs
